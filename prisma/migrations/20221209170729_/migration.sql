b4c09661110c447720bff14e9d6ca0efbd2012f4
jiseong-choi <jiseong1951@kakao.com>
2022-12-10 06:09:07 +0900

    feat: mainPortfolio management

---
+++ prisma/migrations/20221209170729_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Portfolio` ADD COLUMN `isMain` BOOLEAN NOT NULL DEFAULT false;
--- src/controllers/portfolio.ts
+++ src/controllers/portfolio.ts
@@ -47,3 +47,54 @@ export const postPortfolio: ExpressController = async (req, res, next) => {
     res.status(200).json({ message: 'OK', Portfolio });
     }
 }
+
+export const getMainPortfoliosAPI: ControllerAPI = {
+    tags: ['PORTFOLIO'],
+    summary: '메인 포트폴리오 목록 조회 API',
+    path: '/portfolios/main',
+    method: 'GET',
+};
+
+export const getMainPortfolios: ExpressController = async (req, res, next) => {
+    const Portfolios = await database.portfolio.findMany({
+        where: {
+            isMain: true,
+        },
+    });
+    if(!Portfolios) {
+        res.status(404).json({ message: 'Portfolios not found' });
+    } else {
+    res.status(200).json({ message: 'OK', Portfolios });
+    }
+}
+
+export const patchMainPortfolioAPI: ControllerAPI = {
+    tags: ['PORTFOLIO'],
+    summary: '메인 포트폴리오로 수정 API',
+    path: '/portfolio/main',
+    method: 'PATCH',
+    body: [
+        { key: 'id', type: 'number', example: 1 },
+        { key: 'isMain', type: 'boolean', example: true}
+    ],
+    auth: 'jwt',
+    middlewares: [jsonWebTokenMiddleware, Authorization.ADMIN],
+};
+
+export const patchMainPortfolio: ExpressController = async (req, res, next) => {
+    const { id,isMain } = req.body;
+    const Portfolio = await database.portfolio.update({
+        where: {
+            id,
+        },
+        data: {
+            isMain
+        },
+    });
+    if(!Portfolio) {
+        res.status(404).json({ message: 'Portfolio not found' });
+    } else {
+    res.status(200).json({ message: 'OK', Portfolio });
+    }
+}
+
